9ca5b1a525883325a68dbaae68a95ff16b98d38c
Saa-saa <k.o_sisami@hotmail.com>
2026-05-08 11:19:42 +0100

    clean up of code

--- routes/stats-route.js
+++ routes/stats-route.js
@@ -1,30 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const statsController = require("../controllers/stats.controller");
-
-// // CREATE
-// router.post("/", statsController.createStats);
-
-// // READ all
-// router.get("/", statsController.getAllStats);
-
-// // READ one by ID
-// router.get("/:id", statsController.getStatsById);
-
-// // UPDATE by ID
-// router.put("/:id", statsController.updateStats);
-
-// // DELETE by ID
-// router.delete("/:id", statsController.deleteStats);
-
-// // in routes/stats-route.js
-// router.get("/latest", statsController.getAllStats);
-
-// module.exports = router;
-
-
-
-
 const express = require("express");
 const router = express.Router();
 const statsController = require("../controllers/stats.controller");
