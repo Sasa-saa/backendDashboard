d4a7ba7fa02f1ceea00d60e7a3bf0539d4ef93ac
Saa-saa <k.o_sisami@hotmail.com>
2026-05-08 12:22:09 +0100

    server.js file change

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
