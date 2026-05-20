a11e2737b59342a56ae66cceb00c1eedc6c394f2
Saa-saa <k.o_sisami@hotmail.com>
2026-05-20 22:50:30 +0100

    fix: correct classes route path

--- routes/classes-route.js
+++ routes/classes-route.js
@@ -1,11 +1,25 @@
+// const express = require("express");
+// const router = express.Router();
+// const classesController = require("../controllers/classes.controller");
+
+// router.post("/classes", classesController.createClass);
+// router.get("/classes", classesController.getAllClasses);
+// router.get("/classes/:id", classesController.getClassById);
+// router.put("/classes/:id", classesController.updateClass);
+// router.delete("/classes/:id", classesController.deleteClass);
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
 const classesController = require("../controllers/classes.controller");
 
-router.post("/classes", classesController.createClass);
-router.get("/classes", classesController.getAllClasses);
-router.get("/classes/:id", classesController.getClassById);
-router.put("/classes/:id", classesController.updateClass);
-router.delete("/classes/:id", classesController.deleteClass);
+// Remove the extra "/classes" prefix
+router.post("/", classesController.createClass);
+router.get("/", classesController.getAllClasses);
+router.get("/:id", classesController.getClassById);
+router.put("/:id", classesController.updateClass);
+router.delete("/:id", classesController.deleteClass);
 
 module.exports = router;
