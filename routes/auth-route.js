90d1e60f391b80a61e54aacc58a81896bea9bc09
Saa-saa <k.o_sisami@hotmail.com>
2026-05-27 02:20:31 +0100

    fix & clean-up: added logic then refactored codes and removed junk codes

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -11,11 +11,9 @@ const {
 } = require("../controllers/auth.controller");
 
 const { authMiddleware, authCookie } = require("../middleware/auth.middleware");
-// const automationBypass = require("../middleware/automationBypass"); // import bypass middleware
 
 // Public routes
 router.post("/register", registerUser); // ✅ protected by bypass secret
-// router.post("/login", authCookie, loginUser); // ✅ protected by authCookie middleware
 router.post("/login", loginUser);
 router.post("/logout", authCookie, logoutUser);
 
@@ -24,9 +22,5 @@ router.get("/users", authCookie, getUsers);
 router.get("/users/:id", authCookie, getUser);
 router.put("/users/:id", authCookie, updateUser);
 router.delete("/users/:id", authCookie, deleteUser);
-// router.get("/users", authMiddleware, getUsers);
-// router.get("/users/:id", authMiddleware, getUser);
-// router.put("/users/:id", authMiddleware, updateUser);
-// router.delete("/users/:id", authMiddleware, deleteUser);
 
 module.exports = router;
